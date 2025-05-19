d3829e1afd11c470c119d6994026a40a21e05059
Vedahmath <159173596+Vedahmath@users.noreply.github.com>
2025-05-19 22:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (66.63%) - LeetSync

---
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int x=0, y=0, direction=0;
+          for(char ch : instructions.toCharArray()){
+            if(ch == 'G'){         
+                switch (direction){
+                case 0: y++; break;
+                case 1: x++; break;
+                case 2: y--; break;
+                case 3: x--; break;
+                }
+               
+            }else if(ch == 'L'){
+                direction = direction == 0 ? 3 : (direction-1);
+                
+            }else if(ch == 'R'){
+                direction = (direction + 1)%4;
+            }
+        }
+        if(direction!= 0 || (x == 0 && y == 0)){
+            return true;
+        }
+        return false;
+    }
+}
