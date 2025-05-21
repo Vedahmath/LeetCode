82832ffd354ded12bb829a6b74f732e50d2ef1eb
Vedahmath <159173596+Vedahmath@users.noreply.github.com>
2025-05-21 23:54:14 +0530

    Time: 2 ms (82.96%) | Memory: 46.1 MB (32.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,24 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        if(n == 1){
-            return;
-        }
-        int noZeroIndex =0;
-
-
-        for (int i =0; i<nums.length; i++){
-            if(nums[i] != 0){
-                nums[noZeroIndex] = nums[i];
-                noZeroIndex++;
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[count];
+                nums[count] = temp;
+                count++;
             }
         }
-        
-        while( noZeroIndex < n){
-            nums[noZeroIndex] =0;
-            noZeroIndex++;
-        }
-      
         
     }
 }
