b90dd9e9c39903911e6a4149a0b8ed9619ae4f34
Veda Hmath <159173596+Vedahmath@users.noreply.github.com>
2026-02-08 23:59:54 +0530

    Time: 5 ms (33.99%) | Memory: 47.2 MB (10.03%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,35 +1,27 @@
 class Solution {
     public int romanToInt(String s) {
-        int ans = 0, num = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    num = 1;
-                    break;
-                case 'V':
-                    num = 5;
-                    break;
-                case 'X':
-                    num = 10;
-                    break;
-                case 'L':
-                    num = 50;
-                    break;
-                case 'C':
-                    num = 100;
-                    break;
-                case 'D':
-                    num = 500;
-                    break;
-                case 'M':
-                    num = 1000;
-                    break;
+        HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
+        hm.put('I',1);
+        hm.put('V',5);
+        hm.put('X',10);
+        hm.put('L',50);
+        hm.put('C',100);
+        hm.put('D',500);
+        hm.put('M',1000);
+
+        int result = hm.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2; i>=0; i--)
+        {
+            if(hm.get(s.charAt(i))< hm.get(s.charAt(i+1)))
+            {
+                result = result-hm.get(s.charAt(i));
+            }else
+            
+            {
+                 result = result+hm.get(s.charAt(i));
             }
-            if (4 * num < ans)
-                ans -= num;
-            else
-                ans += num;
         }
-        return ans;
+        return result;
+
     }
 }
